c987d87db0ff49bc820b9909ab424d210605e409
alina-sadykova <asadykovaa@gmail.com>
2025-05-17 13:57:28 -0500

    add login page test

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress"); // CommonJS
+// import { defineConfig } from 'cypress'; // ES6
+require("dotenv").config();
+// import 'dotenv' from '' // ES6
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  env: {
+    APP_BASE_URL: process.env.APP_BASE_URL,
+    USERNAME: process.env.USERNAME,
+    PASSWORD: process.env.PASSWORD,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
