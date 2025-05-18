f2d67348e1799bc28fdcbd056c70c16ae8d85ccc
alina-sadykova <asadykovaa@gmail.com>
2025-05-18 10:45:17 -0500

    added more login tests

--- cypress/e2e/integration/login.cy.js
+++ cypress/e2e/integration/login.cy.js
@@ -6,9 +6,10 @@ describe("Login Verification", () => {
   const loginPage = new LoginPage();
   const basePage = new BasePage();
 
-  it("Validate login with valid credentials", () => {
+  beforeEach(() => {
     cy.visit(Cypress.env("APP_BASE_URL"));
-
+  });
+  it("TG11S-T131 - Validate login with valid credentials", () => {
     loginPage.login(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
 
     cy.url().should("include", "weborders");
@@ -18,5 +19,46 @@ describe("Login Verification", () => {
     basePage
       .getWelcomeUserInfo()
       .should("include.text", Cypress.env("USERNAME"));
+  });
+
+  it("TG11S-T142 - Validate logout", () => {
+    loginPage.login(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+    basePage.getLogoutButton().click();
+    cy.url().should("include", "Login");
+    loginPage.getLoginForm().should("be.visible");
+  });
+
+  [
+    {
+      title: "emptyUsernameAndPassword",
+      username: "",
+      password: "",
+    },
+    {
+      title: "invalidUserNameValidPassword",
+      username: "InvalidUsername",
+      password: Cypress.env("PASSWORD"),
+    },
+    {
+      title: "validUsernameAndInvalidPassword",
+      username: Cypress.env("USERNAME"),
+      password: "InvalidPassword",
+    },
+    {
+      title: "invalidUsernameAndInvalidPassword",
+      username: "InvalidUsername",
+      password: "InvalidPassword",
+    },
+  ].forEach((creds) => {
+    it(`TG11S-T164 - Validate login with ${creds.title} `, () => {
+      if (creds.username === "" && creds.password === "")
+        loginPage.clickOnLoginButton();
+      else loginPage.login(creds.username, creds.password);
+
+      cy.url().should("include", "login");
+      loginPage
+        .getErrorMessage()
+        .should("have.text", "Invalid Login or Password.");
+    });
   });
 });
